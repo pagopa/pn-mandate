1dcdf3fc1bb86c554e8b78ec80a2b03975fc8efa
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-03-29 17:04:06 +0200

    fix cors

--- src/main/java/it/pagopa/pn/mandate/CorsGlobalConfiguration.java
+++ src/main/java/it/pagopa/pn/mandate/CorsGlobalConfiguration.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 
 @Configuration
+@Slf4j
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
     @Value("${cors.allowed.domains:}")
@@ -16,6 +19,10 @@ public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
+        
+        if (log.isInfoEnabled())
+            log.info("allowed domains:" + String.join(", ", corsAllowedDomains));
+    
         corsRegistry.addMapping("/**")
                 .allowedOrigins( corsAllowedDomains.toArray( new String[0] ) )
                 .allowedMethods("GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE", "PATCH")

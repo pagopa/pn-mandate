be3dec8cff8968a03e99c8d43e454a58025c56cf
Fabrizio Lana <fabrizio.lana@necsy.it>
2022-06-22 08:46:26 +0200

    conf condivisa

--- src/main/java/it/pagopa/pn/mandate/MandateApplication.java
+++ src/main/java/it/pagopa/pn/mandate/MandateApplication.java
@@ -6,11 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "PN Mandate u-service", version = "1.0", description = "Documentation APIs v1.0"))
 public class MandateApplication {
 
 	public static void main(String[] args) {
--- src/main/java/it/pagopa/pn/mandate/config/PnMandateConfig.java
+++ src/main/java/it/pagopa/pn/mandate/config/PnMandateConfig.java
@@ -1,14 +1,17 @@
 package it.pagopa.pn.mandate.config;
 
+import it.pagopa.pn.commons.conf.SharedAutoConfiguration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "pn.mandate")
+@Import(SharedAutoConfiguration.class)
 public class PnMandateConfig {
 
     private String clientInfopaBasepath;

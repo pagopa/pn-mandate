35e0a09bd013f5f4a3e4f6294edf0b9a6c363942
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-02-01 14:42:33 +0100

    Pn-3829 fix merge

--- src/test/resources/testcontainers/init.sh
+++ src/test/resources/testcontainers/init.sh
@@ -58,20 +58,6 @@ aws --profile default --region us-east-1 --endpoint-url=http://localstack:4566 \
     --provisioned-throughput \
         ReadCapacityUnits=10,WriteCapacityUnits=5
 
-echo "### CREATE QUEUES FIFO ###"
-queues_fifo="local-mandate-expired-mandates.fifo"
 
-for qn in  $( echo $queues_fifo | tr " " "\n" ) ; do
-
-    echo creating queue fifo $qn ...
-
-    aws --profile default --region us-east-1 --endpoint-url http://localstack:4566 \
-        sqs create-queue \
-        --attributes '{"DelaySeconds":"2","FifoQueue": "true","ContentBasedDeduplication": "true"}' \
-        --queue-name $qn
-
-
-
-done
 
 echo "Initialization terminated"

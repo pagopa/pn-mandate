291eba4208222fe429e22d4cb1013b95b47674fe
vincenzo-racca-pa <vincenzo.racca@pagopa.it>
2023-02-02 13:57:04 +0100

    PN-3829 consumer sincrono

--- src/test/java/it/pagopa/pn/mandate/middleware/queue/consumer/ExpiredMandatesHandlerTest.java
+++ src/test/java/it/pagopa/pn/mandate/middleware/queue/consumer/ExpiredMandatesHandlerTest.java
@@ -5,6 +5,7 @@ import it.pagopa.pn.mandate.exceptions.PnMandateNotFoundException;
 import it.pagopa.pn.mandate.middleware.db.entities.MandateEntity;
 import it.pagopa.pn.mandate.middleware.queue.consumer.event.PnMandateExpiredEvent;
 import it.pagopa.pn.mandate.services.mandate.v1.MandateService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,9 @@ import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @FunctionalSpringBootTest
@@ -29,27 +29,61 @@ class ExpiredMandatesHandlerTest {
     @MockBean
     private MandateService mandateService;
 
+//    @Test
+//    void consumeMessageOK() {
+//        Function<Flux<Message<PnMandateExpiredEvent.Payload>>, Mono<Void>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
+//        PnMandateExpiredEvent.Payload payload = PnMandateExpiredEvent.Payload.builder()
+//                .mandateId("fb521b11-202d-452f-944e-88b1eb1c34bd")
+//                .delegatorInternalUserid("PF-12345")
+//                .build();
+//        Message<PnMandateExpiredEvent.Payload> message = MessageBuilder.withPayload(payload).build();
+//        Mockito.when(mandateService.expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345"))
+//                        .thenReturn(Mono.just(new MandateEntity()));
+//
+//        Mono<Void> result = consumer.apply(Flux.just(message));
+//        StepVerifier.create(result)
+//                        .expectComplete()
+//                                .verify();
+//        Mockito.verify(mandateService).expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345");
+//    }
+//
+//    @Test
+//    void consumeMessageKO() {
+//        Function<Flux<Message<PnMandateExpiredEvent.Payload>>, Mono<Void>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
+//        PnMandateExpiredEvent.Payload payload = PnMandateExpiredEvent.Payload.builder()
+//                .mandateId("fb521b11-202d-452f-944e-88b1eb1c34bd")
+//                .delegatorInternalUserid("PF-12345")
+//                .build();
+//        Message<PnMandateExpiredEvent.Payload> message = MessageBuilder.withPayload(payload).build();
+//        Mockito.when(mandateService.expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345"))
+//                .thenReturn(Mono.error(new PnMandateNotFoundException()));
+//
+//        Mono<Void> result = consumer.apply(Flux.just(message));
+//        StepVerifier.create(result)
+//                .expectError(PnMandateNotFoundException.class)
+//                .verify();
+//
+//        Mockito.verify(mandateService).expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345");
+//    }
+
     @Test
     void consumeMessageOK() {
-        Function<Flux<Message<PnMandateExpiredEvent.Payload>>, Mono<Void>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
+        Consumer<Message<PnMandateExpiredEvent.Payload>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
         PnMandateExpiredEvent.Payload payload = PnMandateExpiredEvent.Payload.builder()
                 .mandateId("fb521b11-202d-452f-944e-88b1eb1c34bd")
                 .delegatorInternalUserid("PF-12345")
                 .build();
         Message<PnMandateExpiredEvent.Payload> message = MessageBuilder.withPayload(payload).build();
         Mockito.when(mandateService.expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345"))
-                        .thenReturn(Mono.just(new MandateEntity()));
+                .thenReturn(Mono.just(new MandateEntity()));
 
-        Mono<Void> result = consumer.apply(Flux.just(message));
-        StepVerifier.create(result)
-                        .expectComplete()
-                                .verify();
+        consumer.accept(message);
         Mockito.verify(mandateService).expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345");
     }
 
     @Test
     void consumeMessageKO() {
-        Function<Flux<Message<PnMandateExpiredEvent.Payload>>, Mono<Void>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
+        Consumer<Message<PnMandateExpiredEvent.Payload>> consumer = functionCatalog.lookup(Function.class, "pnMandateExpiredMandatesConsumer");
         PnMandateExpiredEvent.Payload payload = PnMandateExpiredEvent.Payload.builder()
                 .mandateId("fb521b11-202d-452f-944e-88b1eb1c34bd")
                 .delegatorInternalUserid("PF-12345")
@@ -58,11 +92,7 @@ class ExpiredMandatesHandlerTest {
         Mockito.when(mandateService.expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345"))
                 .thenReturn(Mono.error(new PnMandateNotFoundException()));
 
-        Mono<Void> result = consumer.apply(Flux.just(message));
-        StepVerifier.create(result)
-                .expectError(PnMandateNotFoundException.class)
-                .verify();
-
+        Assertions.assertThrows(PnMandateNotFoundException.class, () ->  consumer.accept(message));
         Mockito.verify(mandateService).expireMandate("fb521b11-202d-452f-944e-88b1eb1c34bd", "PF-12345");
     }
 

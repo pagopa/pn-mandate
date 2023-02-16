55843c9d823884ad64d29d97774eb6777ee0f13e
Simone Martelli <simone.martelli@emea.nttdata.com>
2023-02-16 18:45:20 +0100

    fix mapping forbidden to HTTP 404

--- src/main/java/it/pagopa/pn/mandate/exceptions/PnForbiddenException.java
+++ src/main/java/it/pagopa/pn/mandate/exceptions/PnForbiddenException.java
@@ -3,13 +3,13 @@ package it.pagopa.pn.mandate.exceptions;
 import it.pagopa.pn.commons.exceptions.PnRuntimeException;
 import org.springframework.http.HttpStatus;
 
-import static it.pagopa.pn.mandate.exceptions.PnMandateExceptionCodes.ERROR_CODE_FORBIDDEN;
+import static it.pagopa.pn.mandate.exceptions.PnMandateExceptionCodes.ERROR_CODE_MANDATE_NOT_FOUND;
 
 public class PnForbiddenException extends PnRuntimeException {
 
     public PnForbiddenException() {
         super("Accesso negato!", "L'utente non è autorizzato ad accedere alla risorsa richiesta.",
-                HttpStatus.FORBIDDEN.value(), ERROR_CODE_FORBIDDEN, null, null);
+                HttpStatus.NOT_FOUND.value(), ERROR_CODE_MANDATE_NOT_FOUND, null, null);
     }
 
 }

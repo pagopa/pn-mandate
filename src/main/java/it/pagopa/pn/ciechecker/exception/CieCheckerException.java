5539115d157752fb50fb473898b042780cf2c615
damiano.lozzi <damianolozzi1989@gmail.com>
2025-08-25 12:02:15 +0200

    PN-16047: moved ciechecker to main branch

--- src/main/java/it/pagopa/pn/ciechecker/exception/CieCheckerException.java
+++ src/main/java/it/pagopa/pn/ciechecker/exception/CieCheckerException.java
@@ -2,5 +2,7 @@ package it.pagopa.pn.ciechecker.exception;
 
 public class CieCheckerException extends RuntimeException{
     public CieCheckerException(){super();}
+    public CieCheckerException(String message){super(message);}
+    public CieCheckerException(Exception cause){super(cause);}
 
 }

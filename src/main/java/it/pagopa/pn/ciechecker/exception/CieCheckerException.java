78579ba5e8b3973b243df0dd63aabd59f99ad950
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2025-09-12 14:26:25 +0200

    PN-16048-bis - Change return object in Enum 'ResultCieChecker' for method verifyDigitalSignature (#219)

--- src/main/java/it/pagopa/pn/ciechecker/exception/CieCheckerException.java
+++ src/main/java/it/pagopa/pn/ciechecker/exception/CieCheckerException.java
@@ -1,8 +1,22 @@
 package it.pagopa.pn.ciechecker.exception;
 
+import it.pagopa.pn.ciechecker.model.ResultCieChecker;
+
 public class CieCheckerException extends RuntimeException{
+
+    private ResultCieChecker result;
+
     public CieCheckerException(){super();}
     public CieCheckerException(String message){super(message);}
     public CieCheckerException(Exception cause){super(cause);}
+
+    public CieCheckerException(ResultCieChecker result) {
+        super(result.getValue());
+        this.result = result;
+    }
+
+    public ResultCieChecker getResult() {
+        return result;
+    }
 
 }

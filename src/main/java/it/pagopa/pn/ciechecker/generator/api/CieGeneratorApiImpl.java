0440dd4ab4d2f1a0ea4fa8113a3e1bcd0d42f192
damiano.lozzi <damianolozzi1989@gmail.com>
2025-10-30 15:58:14 +0100

    PN-16717 - refactor CieGeneratorApiImpl

--- src/main/java/it/pagopa/pn/ciechecker/generator/api/CieGeneratorApiImpl.java
+++ src/main/java/it/pagopa/pn/ciechecker/generator/api/CieGeneratorApiImpl.java
@@ -8,6 +8,8 @@ import it.pagopa.pn.ciechecker.generator.ias.IasBuilder;
 import it.pagopa.pn.ciechecker.generator.loader.CertAndKeyLoader;
 import it.pagopa.pn.ciechecker.generator.model.CertAndKey;
 import it.pagopa.pn.ciechecker.generator.model.CieCaAndKey;
+import it.pagopa.pn.ciechecker.generator.model.Issuer;
+import it.pagopa.pn.ciechecker.generator.pki.CiePki;
 import it.pagopa.pn.ciechecker.generator.sod.SodMrtdBuilder;
 import it.pagopa.pn.ciechecker.model.CieIas;
 import it.pagopa.pn.ciechecker.model.CieMrtd;
@@ -15,33 +17,44 @@ import it.pagopa.pn.ciechecker.model.CieValidationData;
 import it.pagopa.pn.ciechecker.model.ResultCieChecker;
 import it.pagopa.pn.ciechecker.utils.LogsConstant;
 import lombok.extern.slf4j.Slf4j;
-
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Set;
 
 
-@lombok.CustomLog
+@Slf4j
 public class CieGeneratorApiImpl implements CieGeneratorApi {
+
+    CertAndKeyLoader certAndKeyLoader = new CertAndKeyLoader();
 
 
     @Override
     public CieValidationData generateCieValidationData(Path outputDir,
-                                                       String codiceFiscale,
+                                                       String codiceFiscaleDelegante,
+                                                       String codiceFiscaleCIE,
                                                        LocalDate expirationDate,
                                                        String nonce) throws CieCheckerException {
 
         try {
+            CiePki pki = new CiePki();
+
             //recupero cert e key
-            CertAndKey issuerCertAndKeyFromS3 = new CertAndKeyLoader(
-            ).loadIssuerCertAndKeyFromS3();
+            CertAndKey issuerCertAndKeyFromS3 = certAndKeyLoader.loadIssuerCertAndKeyFromS3();
+
+
+            //costruisco document signer
+            CertAndKey userCertificate = pki.issueDocumentSigner(
+                    new Issuer(issuerCertAndKeyFromS3.getCertificate(), issuerCertAndKeyFromS3.getPrivateKey()),
+                    2048,
+                    365
+            );
 
 
             IasBuilder iasBuilder = new IasBuilder();
             // creazione ias
             CieIas ias = iasBuilder.createCieIas(
                     iasBuilder.generateNisNumericString(IasBuilder.DEFAULT_NIS_LEN).getBytes(),     //NIS
-                    issuerCertAndKeyFromS3.getEncodedPublicKey(),                                   //PUBKEY
+                    userCertificate.getEncodedPublicKey(),                                   //PUBKEY
                     issuerCertAndKeyFromS3.getPrivateKey(),                                         //PRVKEY
                     issuerCertAndKeyFromS3.getCertificate()                                         //CERT
             );
@@ -53,8 +66,8 @@ public class CieGeneratorApiImpl implements CieGeneratorApi {
                     CieGeneratorConstants.DEFAULT_NATIONALITY,
                     CieGeneratorConstants.DEFAULT_DATE_OF_BIRTH,
                     CieGeneratorConstants.DEFAULT_SEX,
-                    CieGeneratorConstants.DEFAULT_EXPIRY_DATE,
-                    codiceFiscale,
+                    expirationDate,
+                    codiceFiscaleCIE,
                     CieGeneratorConstants.DEFAULT_PLACE_OF_BIRTH,
                     issuerCertAndKeyFromS3.getPrivateKey(),
                     issuerCertAndKeyFromS3.getCertificate()
@@ -67,28 +80,35 @@ public class CieGeneratorApiImpl implements CieGeneratorApi {
             validationData.setCieMrtd(mrtd);
             validationData.setCieIas(ias);
             validationData.setNonce(nonce);
-            validationData.setCodFiscDelegante(codiceFiscale);
+            validationData.setCodFiscDelegante(codiceFiscaleDelegante);
 
-            validationData.setSignedNonce(ChallengeResponseBuilder.signNonce(nonce,issuerCertAndKeyFromS3.getPrivateKey()));
+            validationData.setSignedNonce(ChallengeResponseBuilder.signNonce(
+                    nonce,
+                    userCertificate.getPrivateKey())
+            );
 
             // CONVERT TO CIECAANDKEY
             CieCaAndKey cieCaAndkey = new CieCaAndKey();
-            cieCaAndkey.setCertPem(issuerCertAndKeyFromS3.getEncodedCertificate());
-            cieCaAndkey.setCertKey(issuerCertAndKeyFromS3.getEncodedPublicKey());
+            cieCaAndkey.setCertPem(userCertificate.getEncodedCertificate());
+            cieCaAndkey.setCertKey(userCertificate.getEncodedPrivateKey());
             //
 
             //EXPORT FILES
-            final CieFilesExporter generator = new CieFilesExporter(validationData,cieCaAndkey,outputDir.toAbsolutePath().toString());
+            final CieFilesExporter generator = new CieFilesExporter(
+                    validationData,
+                    cieCaAndkey,
+                    outputDir.toAbsolutePath().toString()
+            );
+
             final Set<String> exportedKeys = generator.exportCieArtifactsToFiles().keySet();
             exportedKeys.forEach(key ->
-                log.debug("Exported file: {} ",key)
+                    log.debug("Exported file: {} ", key)
             );
             //
             return validationData;
-        }catch (Exception e ){
-            log.error(Exception.class + LogsConstant.MESSAGE  + e.getMessage());
+        } catch (Exception e) {
+            log.error(Exception.class + LogsConstant.MESSAGE + e.getMessage());
             throw new CieCheckerException(ResultCieChecker.KO, e);
         }
     }
-
 }

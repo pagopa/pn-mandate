bdae8d820259e528cc790ac26dbcc785e0fe25c6
Andrea Bertucci <andrea.bertucci@pagopa.it>
2025-12-17 19:41:46 +0100

    chore: updated openapi for test

--- src/main/java/it/pagopa/pn/mandate/springbootcfg/JacksonConfig.java
+++ src/main/java/it/pagopa/pn/mandate/springbootcfg/JacksonConfig.java
@@ -1,7 +1,6 @@
 package it.pagopa.pn.mandate.springbootcfg;
 
 import it.pagopa.pn.ciechecker.utils.TestMixIn;
-import it.pagopa.pn.common.rest.error.v1.dto.Problem;
 import it.pagopa.pn.common.rest.error.v1.dto.ProblemError;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,7 @@ public class JacksonConfig {
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
         Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
         // Associa il Mix-In alla classe generata dal plugin
-        b.mixIn(Problem.class, TestMixIn.class);
+        b.mixIn(ProblemError.class, TestMixIn.class);
         return b;
     }
 }
